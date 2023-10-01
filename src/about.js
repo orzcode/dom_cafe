8af12f162979854fa5a88b63ad7b866291c37c1c
orzcode <71000311+orzcode@users.noreply.github.com>
2023-10-01 17:58:55 +1300

    phew

--- src/about.js
+++ src/about.js
@@ -1,6 +1,36 @@
-const about = () => {
+const aboutLink = () => {
 
+	console.log("about.js loaded!")
+	const aboutDiv = document.createElement("div");
+	aboutDiv.id = "about";
+	aboutDiv.className = "tabcontent";
+  
+	const img = document.createElement("img");
+	img.src = "/images/jungle_cuisine.jpg";
+  
+	const h2 = document.createElement("h2");
+	h2.innerHTML = "<strong><em>From lilypad to lilyplate </em></strong>🌺🐸";
+  
+	const p1 = document.createElement("p");
+	p1.textContent =
+	  "Unearth the enchanting flavors of Frog Cafe, Lima's flavorful oasis since 2019. Nestled amidst the vibrant cityscape, our jungle-themed haven is where healthy meets wild. We're passionate about crafting nourishing dishes that take your taste buds on a journey into the heart of the Amazon.";
+  
+	const p2 = document.createElement("p");
+	p2.textContent =
+	  "Our menu is a celebration of nutritionally functional ingredients and powerful plants traditionally used by Amazonian communities. From vibrant salads bursting with superfoods to tropical smoothie bowls brimming with vitality, each bite is a tribute to the wisdom of the rainforest.";
+  
+	const p3 = document.createElement("p");
+	p3.textContent =
+	  "As you savor our dishes, you'll be surrounded by lush greenery, creating an immersive dining experience that transports you straight into the jungle. Join us at Frog Cafe for a culinary adventure like no other, where fresh, wholesome, and delicious food awaits. 🌿🐸🍴🌱";
+  
+	aboutDiv.appendChild(img);
+	aboutDiv.appendChild(h2);
+	aboutDiv.appendChild(p1);
+	aboutDiv.appendChild(p2);
+	aboutDiv.appendChild(p3);
 
+	const contentDiv = document.getElementById("content");
+	contentDiv.appendChild(aboutDiv);
 }
 
-export default { about }
+export default aboutLink
--- src/index.js
+++ src/index.js
@@ -1,6 +1,53 @@
-import _ from 'lodash';
-import scaffold from './scaffold.js';
+import _ from "lodash";
+import scaffold from "./scaffold.js";
 //scaffold()
 
-import about from './about.js';
-//about()
+import aboutLink from "./about.js";
+// document.getElementById("aboutLink");
+// aboutLink.addEventListener("click", function(event) {
+// 	const links = document.querySelectorAll('.tablinks a');
+// 	links.forEach(l => l.classList.remove('active'));
+
+//     // Add the 'active' class to the clicked link
+//     this.classList.add('active');
+// 	event.preventDefault(); // Prevent the default link behavior (page refresh)
+
+// 	// Call the about() function when the link is clicked
+// 	about();
+//   });
+
+// Get a reference to the parent container that holds the tablinks
+const tabLinksContainer = document.querySelector("nav ul");
+
+// Add a click event listener to the parent container
+tabLinksContainer.addEventListener("click", function (event) {
+  const clickedLink = event.target;
+
+  event.preventDefault(); // Prevent the default link behavior (page refresh)
+
+// Get a reference to the #content div
+const contentDiv = document.getElementById("content");
+
+// Remove all content inside the #content div
+contentDiv.innerHTML = "";
+
+  // Remove the 'active' class from all tablinks
+  const links = this.querySelectorAll("a");
+  links.forEach((link) => link.classList.remove("active"));
+
+  // Add the 'active' class to the clicked link
+  clickedLink.classList.add("active");
+
+  // Call the corresponding function (e.g., about, menu) based on the link's ID
+  switch (clickedLink.id) {
+    case "aboutLink":
+      aboutLink();
+      break;
+    case "menuLink":
+      menuLink();
+      break;
+    case "contactLink":
+      contactLink();
+      break;
+  }
+});
